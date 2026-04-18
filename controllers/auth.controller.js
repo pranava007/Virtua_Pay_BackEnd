bb7bfa5eab76a78fe18fd261c871338468d45f1a
pranava007 <pranavamuthu000@gmail.com>
2026-04-18 22:02:23 +0530

    add

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@
 
 import { getAccessToken } from "../services/shopify.service.js";
 import Store from "../models/Store.model.js";
+import crypto from "crypto";
 
 export const installApp = (req, res) => {
   const { shop } = req.query;
@@ -11,12 +12,25 @@ export const installApp = (req, res) => {
   res.redirect(url);
 };
 
+
 export const authCallback = async (req, res) => {
   try {
-    const { shop, code } = req.query;
+    const { shop, code, hmac, ...rest } = req.query;
 
     if (!shop || !code) {
       return res.status(400).send("Missing shop or code ❌");
+    }
+
+    // 🔥 HMAC VALIDATION
+    const message = new URLSearchParams(rest).toString();
+
+    const generatedHash = crypto
+      .createHmac("sha256", process.env.SHOPIFY_API_SECRET)
+      .update(message)
+      .digest("hex");
+
+    if (generatedHash !== hmac) {
+      return res.status(400).send("HMAC validation failed ❌");
     }
 
     const data = await getAccessToken(shop, code);
@@ -30,7 +44,7 @@ export const authCallback = async (req, res) => {
     res.send("App Installed Successfully ✅");
 
   } catch (err) {
-    console.error(err);
+    console.error("❌ ERROR:", err);
     res.status(500).send(err.message);
   }
 };
