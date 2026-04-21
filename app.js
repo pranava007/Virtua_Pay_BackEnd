16f1195049e919207650c922b881aaa22bbe424b
pranava007 <pranavamuthu000@gmail.com>
2026-04-21 08:40:58 +0530

    added last config

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import cors from "cors"; // ✅ ADD THIS
 import authRoutes from "./routes/auth.routes.js";
 import webhookRoutes from "./routes/webhook.routes.js";
 import paymentRoutes from "./routes/payment.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
 
 dotenv.config();
 
@@ -25,6 +26,8 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/webhook", webhookRoutes);
 app.use("/payment", paymentRoutes);
+app.use("/admin", adminRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("Payment Gateway SaaS Running 🚀");
