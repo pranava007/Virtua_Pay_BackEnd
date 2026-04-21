818a34ee7c9374d4e11b4c6f237458bea52dac64
pranava007 <pranavamuthu000@gmail.com>
2026-04-21 08:54:35 +0530

    add

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -14,18 +14,26 @@ export const getGatewayConfig = async (req, res) => {
   }
 };
 
+
 export const updateGatewayConfig = async (req, res) => {
   try {
+    console.log("BODY:", req.body);        // 👈 ADD
+    console.log("USER:", req.user);        // 👈 ADD
+
     const { activeGateway } = req.body;
+
     let config = await GatewayConfig.findOne();
+
     if (config) {
       config.activeGateway = activeGateway;
       await config.save();
     } else {
       config = await GatewayConfig.create({ activeGateway });
     }
+
     res.json(config);
   } catch (err) {
+    console.log("ERROR:", err); // 👈 IMPORTANT
     res.status(500).json({ error: err.message });
   }
 };
